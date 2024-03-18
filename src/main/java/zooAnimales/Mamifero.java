f358fd72952656a76b3036d73c3d6b065dbfdd5a
Jordan-677 <159493664+Jordan-677@users.noreply.github.com>
2024-03-18 00:16:48 -0500

    estructura
    creando la estructura del programa

---
+++ src/main/java/zooAnimales/Mamifero.java
@@ -0,0 +1,21 @@
+package zooAnimales;
+
+public class Mamifero extends Animal {
+	Mamifero[] listado;
+	int caballos;
+	int leones;
+	boolean pelaje;
+	int patas;
+	
+	public void cantidadMamiferos() {
+		
+	}
+	
+	public void crearCaballo() {
+		
+	}
+	
+	public void crearLeon() {
+			
+		}
+}
